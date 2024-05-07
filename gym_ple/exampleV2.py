bcc06fac0495b89ec2b925d0aa04788413646e9a
SupporterDog <68847588+SupporterDog@users.noreply.github.com>
2024-05-07 22:58:57 +0900

    Update exampleV2.py

--- gym_ple/exampleV2.py
+++ gym_ple/exampleV2.py
@@ -15,6 +15,48 @@ from tensorflow.keras.optimizers import Adam
 
 
 # The world's simplest agent!
+class DQNAgent:
+    def __init__(self, state_size, action_size):
+        self.state_size = state_size
+        self.action_size = action_size
+        self.memory = deque(maxlen=2000)
+        self.gamma = 0.95    # discount rate
+        self.epsilon = 1.0  # exploration rate
+        self.epsilon_min = 0.01
+        self.epsilon_decay = 0.995
+        self.learning_rate = 0.001
+        self.model = self._build_model()
+
+    def _build_model(self):
+        model = Sequential()
+        model.add(Dense(24, input_dim=self.state_size, activation='relu'))
+        model.add(Dense(24, activation='relu'))
+        model.add(Dense(self.action_size, activation='linear'))
+        model.compile(loss='mse', optimizer=Adam(lr=self.learning_rate))
+        return model
+
+    def remember(self, state, action, reward, next_state, done):
+        self.memory.append((state, action, reward, next_state, done))
+
+    def act(self, state):
+        if np.random.rand() <= self.epsilon:
+            return np.random.choice(self.action_size)
+        act_values = self.model.predict(state)
+        return np.argmax(act_values[0])  
+
+    def replay(self, batch_size):
+        minibatch = np.array(random.sample(self.memory, batch_size))
+        for state, action, reward, next_state, done in minibatch:
+            target = reward
+            if not done:
+                target = (reward + self.gamma *
+                          np.amax(self.model.predict(next_state)[0]))
+            target_f = self.model.predict(state)
+            target_f[0][action] = target
+            self.model.fit(state, target_f, epochs=1, verbose=0)
+        if self.epsilon > self.epsilon_min:
+            self.epsilon *= self.epsilon_decay
+
 class RandomAgent(object):
     def __init__(self, action_space):
         self.action_space = action_space
@@ -42,25 +84,31 @@ if __name__ == '__main__':
     # monitor's seeding creates a new action_space instance with the
     # appropriate pseudorandom number generator.
     env.seed(0)
-    agent = RandomAgent(env.action_space)
+    state_size = env.observation_space.shape[0]
+    action_size = env.action_space.n
 
-    episode_count = 100
-    reward = 0
-    done = False
+    # DQN agent setup
+    agent = DQNAgent(state_size, action_size)
 
-    for i in range(episode_count):
-        ob = env.reset()
-
-        while True:
-            action = agent.act(ob, reward, done)
-            ob, reward, done, _ = env.step(action)
+    batch_size = 32
+    num_episodes = 1000
+    for e in range(num_episodes):
+        state = env.reset()
+        state = np.reshape(state, [1, state_size])
+        for time in range(500):
+            action = agent.act(state)
+            next_state, reward, done, _ = env.step(action)
+            reward = reward if not done else -10
+            next_state = np.reshape(next_state, [1, state_size])
+            agent.remember(state, action, reward, next_state, done)
+            state = next_state
             if done:
+                print("episode: {}/{}, score: {}, e: {:.2}"
+                      .format(e, num_episodes, time, agent.epsilon))
                 break
-            # Note there's no env.render() here. But the environment still can open window and
-            # render if asked by env.monitor: it calls env.render('rgb_array') to record video.
-            # Video is not recorded every episode, see capped_cubic_video_schedule for details.
+            if len(agent.memory) > batch_size:
+                agent.replay(batch_size)
 
-    # Dump result info to disk
     env.close()
 
     # Upload to the scoreboard. We could also do this from another
