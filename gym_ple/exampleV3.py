41d4b07c2d4964fb16581a2b94da3b2827df3ee8
SupporterDog <68847588+SupporterDog@users.noreply.github.com>
2024-05-08 23:26:58 +0900

    Update exampleV3.py
    regenerate

--- gym_ple/exampleV3.py
+++ gym_ple/exampleV3.py
@@ -13,18 +13,18 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.optimizers import Adam
 
+
 class DQNAgent:
     def __init__(self, state_size, action_size):
         self.state_size = state_size
         self.action_size = action_size
-        self.memory = deque(maxlen=1000)  # 메모리 크기를 줄임
+        self.memory = deque(maxlen=2000)
         self.gamma = 0.95    # discount rate
         self.epsilon = 1.0  # exploration rate
         self.epsilon_min = 0.01
         self.epsilon_decay = 0.995
         self.learning_rate = 0.001
         self.model = self._build_model()
-        self.train_interval = 5  # 5 에피소드마다 한 번씩 학습
 
     def _build_model(self):
         model = Sequential()
@@ -44,62 +44,66 @@ class DQNAgent:
         return np.argmax(act_values[0])  
 
     def replay(self, batch_size):
-        if len(self.memory) < batch_size:
-            return
         minibatch = random.sample(self.memory, batch_size)
         for state, action, reward, next_state, done in minibatch:
             target = reward
             if not done:
-                target = (reward + self.gamma * np.amax(self.model.predict(next_state)[0]))
+                target = (reward + self.gamma *
+                          np.amax(self.model.predict(next_state)[0]))
             target_f = self.model.predict(state)
             target_f[0][action] = target
             self.model.fit(state, target_f, epochs=1, verbose=0)
+        if self.epsilon > self.epsilon_min:
+            self.epsilon *= self.epsilon_decay
 
-    def run_episodes(self, env, num_episodes, batch_size):
-        for e in range(num_episodes):
-            state = env.reset()
-            state = np.reshape(state, [1, self.state_size])
-            episode_score = 0
-            for time in range(25):
-                action = self.act(state)
-                next_state, reward, done, _ = env.step(action)
-                reward = reward if not done else -10
-                next_state = np.reshape(next_state, [1, self.state_size])
-                self.memory.append((state, action, reward, next_state, done))
-                state = next_state
-                episode_score += reward
-                if done:
-                    episode_result = "episode: {}/{}, score: {}, e: {:.2}".format(e, num_episodes, time, self.epsilon)
-                    print(episode_result)
-                    with open('/content/result/results.txt', 'a') as f:
-                        f.write(episode_result + '\n')
-                    break
-            if e % self.train_interval == 0:
-                self.replay(batch_size)
-            if self.epsilon > self.epsilon_min:
-                self.epsilon *= self.epsilon_decay
 
 if __name__ == '__main__':
-    # 환경 설정
-    env_name = 'FlappyBird-v0' if len(sys.argv) < 2 else sys.argv[1]
-    env = gym.make(env_name)
+    # You can optionally set up the logger. Also fine to set the level
+    # to logging.DEBUG or logging.WARN if you want to change the
+    # amount of output.
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+
+    env = gym.make('FlappyBird-v0' if len(sys.argv)<2 else sys.argv[1])
+
+    # You provide the directory to write to (can be an existing
+    # directory, including one with existing data -- all monitor files
+    # will be namespaced). You can also dump to a tempdir if you'd
+    # like: tempfile.mkdtemp().
+    video_folder = '/content/video'
+    env = RecordVideo(env, video_folder=video_folder, episode_trigger=lambda x: True)
+
+    # This declaration must go *after* the monitor call, since the
+    # monitor's seeding creates a new action_space instance with the
+    # appropriate pseudorandom number generator.
     env.seed(0)
     state_size = 288 * 512 * 3  # 상태 크기를 수정합니다.
     action_size = env.action_space.n
 
-    # 에이전트 설정
+    # DQN agent setup
     agent = DQNAgent(state_size, action_size)
 
-    # 학습 파라미터 설정
     batch_size = 32
     num_episodes = 25
+    for e in range(num_episodes):
+        state = env.reset()
+        state = np.reshape(state, [1, state_size])
+        for time in range(25):
+            action = agent.act(state)
+            next_state, reward, done, _ = env.step(action)
+            reward = reward if not done else -10
+            next_state = np.reshape(next_state, [1, state_size])
+            agent.remember(state, action, reward, next_state, done)
+            state = next_state
+            if done:
+                print("episode: {}/{}, score: {}, e: {:.2}"
+                      .format(e, num_episodes, time, agent.epsilon))
+                break
+            if len(agent.memory) > batch_size:
+                agent.replay(batch_size)
 
-    # 결과를 저장할 디렉토리 생성
-    result_dir = '/content/result'
-    os.makedirs(result_dir, exist_ok=True)
-
-    # 에피소드 실행 및 학습
-    agent.run_episodes(env, num_episodes, batch_size)
-
-    # 환경 종료
     env.close()
+
+    # Upload to the scoreboard. We could also do this from another
+    # process if we wanted.
+    logger.info("Successfully ran RandomAgent. Now trying to upload results to the scoreboard. If it breaks, you can always just try re-uploading the same results.")
